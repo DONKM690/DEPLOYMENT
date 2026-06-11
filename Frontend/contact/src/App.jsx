cd659a12290d8a007cb8e459902b4a2b6dec0f41
DON <donkm690@gmail.com>
2026-06-11 19:15:41 +0530

    fix api url for production

--- Frontend/contact/src/App.jsx
+++ Frontend/contact/src/App.jsx
@@ -19,16 +19,16 @@ function App() {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const response = await fetch(
-      "http://127.0.0.1:8000/api/contact/",
-      {
-        method:"POST",
-        headers:{
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(formData),
-      }
-    );
+   const response = await fetch(
+  "https://donkm.pythonanywhere.com/api/contact/",
+  {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(formData),
+  }
+);
 
     const data= await response.json();
     setResponseMessage(data.message);
